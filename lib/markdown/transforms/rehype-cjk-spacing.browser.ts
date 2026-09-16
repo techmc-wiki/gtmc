bf556e134fa39c14ff6413d84094d76b62106def
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-16 03:50:13 -0400

    fix(markdown): update pangu api to spaceText for v10

--- lib/markdown/transforms/rehype-cjk-spacing.browser.ts
+++ lib/markdown/transforms/rehype-cjk-spacing.browser.ts
@@ -2,5 +2,5 @@ import { pangu } from "pangu/browser"
 import { createRehypeCJKSpacing } from "./rehype-cjk-spacing-base"
 
 export const rehypeCJKSpacingBrowser = createRehypeCJKSpacing(
-  pangu.spacingText.bind(pangu)
+  pangu.spaceText.bind(pangu)
 )
